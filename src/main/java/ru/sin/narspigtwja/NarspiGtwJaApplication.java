98e68eb7822fa65a7e5f65dde11ad5bab4d5448a
Med771 <mm7737333@gmail.com>
2025-09-16 15:40:30 +0300

    Init commit

---
+++ src/main/java/ru/sin/narspigtwja/NarspiGtwJaApplication.java
@@ -0,0 +1,13 @@
+package ru.sin.narspigtwja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarspiGtwJaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NarspiGtwJaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sin/narspigtwja/NarspiGtwJaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sin.narspigtwja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarspiGtwJaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
