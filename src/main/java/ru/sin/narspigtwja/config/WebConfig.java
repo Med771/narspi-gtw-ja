4d6ca329cacab7f917671d1ccb02502bae7a661a
Med771 <mm7737333@gmail.com>
2025-09-17 15:13:35 +0300

    add config

---
+++ src/main/java/ru/sin/narspigtwja/config/WebConfig.java
@@ -0,0 +1,24 @@
+package ru.sin.narspigtwja.config;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://narspi21.ru", "http://localhost:3000", "http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
